69616cd34ee20c83de9fbaaf66363984994f01e6
Jasonltr <ltr.jason@gmail.com>
2022-02-25 13:25:38 +0800

    shortend python code

--- challenge3/transform.py
+++ challenge3/transform.py
@@ -12,34 +12,18 @@ uniIT = uniIT.set_index(['year','sex','course'])
 polyIT = polyIT.set_index(['year','sex','course'])
 
 #transform data to int
-uniIT['intake']=uniIT['intake'].astype(str)
-uniIT['intake']=uniIT['intake'].str.replace(',','')
+uniIT=uniIT.astype({'intake':'str','enrolment':'str','graduates':'str'})
+uniIT = uniIT.replace(r',','',regex=True)
+uniIT=uniIT.astype({'intake':'int','enrolment':'int','graduates':'int'})
 
-uniIT['enrolment']=uniIT['enrolment'].astype(str)
-uniIT['enrolment']=uniIT['enrolment'].str.replace(',','')
-
-uniIT['graduates']=uniIT['graduates'].astype(str)
-uniIT['graduates']=uniIT['graduates'].str.replace(',','')
-
-uniIT['intake'] = pd.to_numeric(uniIT['intake'])
-uniIT['enrolment'] = pd.to_numeric(uniIT['enrolment'])
-uniIT['graduates'] = pd.to_numeric(uniIT['graduates'])
-
-polyIT['intake']=polyIT['intake'].astype(str)
-polyIT['intake']=polyIT['intake'].str.replace(',','')
-
-polyIT['enrolment']=polyIT['enrolment'].astype(str)
-polyIT['enrolment']=polyIT['enrolment'].str.replace(',','')
-
-polyIT['graduates']=polyIT['graduates'].astype(str)
-polyIT['graduates']=polyIT['graduates'].str.replace(',','')
-
-polyIT['intake'] = pd.to_numeric(polyIT['intake'])
-polyIT['enrolment'] = pd.to_numeric(polyIT['enrolment'])
-polyIT['graduates'] = pd.to_numeric(polyIT['graduates'])
+polyIT=polyIT.astype({'intake':'str','enrolment':'str','graduates':'str'})
+polyIT = polyIT.replace(r',','',regex=True)
+polyIT=polyIT.astype({'intake':'int','enrolment':'int','graduates':'int'})
 
 #add them up
 allIT = uniIT.add(polyIT)
 
 #export to csv
 allIT.to_csv('/home/jason/Documents/GitHub/ansible-training/challenge3/allIT.csv')
+
+print(allIT)
